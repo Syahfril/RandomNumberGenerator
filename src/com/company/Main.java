058f8c9367014acc81534a6c347166cb36343567
William Kurniawan <william@student.ittelkom-sby.ac.id>
2021-11-08 13:48:00 +0700

    Change main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("The Number is " + new RandomNumberGenerator().randomNumber());
+        System.out.println("The Number is " + new RandomNumberGenerator().randomNumber(99));
     }
 }
